dceb763e542c0ad978e17cfb09b907e84dbc3f4f
skyholeBishop <51252247+bigLRRH@users.noreply.github.com>
2022-06-01 23:50:25 +0800

    routine update

--- L1/L1_025_Positive_Int_A_Plus_B.cpp
+++ L1/L1_025_Positive_Int_A_Plus_B.cpp
@@ -1,46 +1,41 @@
 #include <iostream>
 #include <cstdio>
+#include <regex>
+#include <sstream>
 using namespace std;
 
 int main()
 {
-    int a,b;
-    bool flag = true;
-    if(!scanf("%d",&a))
+    //输入
+    string a, b;
+    int A, B;
+    cin >> a;
+    getline(cin, b);
+    b.erase(0, 1);
+    //判断
+    bool flag_a(false), flag_b(false);
+    regex reg(R"(\d{1,4})");
+    istringstream iss;
+    if (regex_match(a, reg))
     {
-        cout<<'?';
-        flag = false;
+        iss.str(a);
+        iss >> A;
+        if (A >= 1 && A <= 1000)
+        {
+            flag_a = true;
+        }
     }
-    else if (a<0)
+    if (regex_match(b, reg))
     {
-        cout<<'?';
-        flag = false;
+        iss.clear();
+        iss.str(b);
+        iss >> B;
+        if (B >= 1 && B <= 1000)
+        {
+            flag_b = true;
+        }
     }
-    else
-    {
-        cout<<a;
-    }
-    
-    cout<<" + ";
-
-    if(!scanf("%d",&b))
-    {
-        cout<<'?';
-        flag = false;
-    }
-    else if (a<0)
-    {
-        cout<<'?';
-        flag = false;
-    }
-    else
-    {
-        cout<<b;
-    }
-
-    if(flag)
-        printf("= %d",a+b);
-    else
-        cout<<"= ?";
+    //输出
+    printf("%s + %s = %s\n", flag_a ? &to_string(A)[0] : &"?"[0], flag_b ? &to_string(B)[0] : &"?"[0], flag_a && flag_b ? &to_string(A + B)[0] : &"?"[0]);
     return 0;
 }
